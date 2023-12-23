aeeac07a420bf94293518a007e70ad1e4fae8068
like-mojang <88312026+like-mojang@users.noreply.github.com>
2023-12-23 18:28:08 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,18 @@
-import sys
-from gunicorn.app.wsgiapp import run
-if __name__ == '__main__':
-    sys.argv = "gunicorn --bind 0.0.0.0:5151 app:app".split()
-    sys.exit(run())
+import botpy, os
+from botpy.message import Message
+
+class MyClient(botpy.Client):
+    async def on_at_message_create(self, message: Message):
+        
+        await self.api.post_message(channel_id=message.channel_id, content='本地文件读写测试:测试通过！')
+
+    async def on_public_message_delete(self, message: Message):
+        """
+        此处为处理该事件的代码
+        """
+
+
+intents = botpy.Intents(public_guild_messages=True,guild_messages=True, direct_message=True, guilds=True, audio_action=True, forums=True, message_audit=True, interaction=True, guild_members=True, guild_message_reactions=True)
+client = MyClient(intents=intents)
+print(dir(MyClient))
+client.run(appid="102059538", secret="pY6XnqgIkz1pSs42")
